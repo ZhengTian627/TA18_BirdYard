fa0f3267cb5c7fcec629ccc90da7b0adfd5afe98
ZhengTian627 <tianzheng0627@outlook.com>
2025-04-03 17:09:28 +1100

    Initialization Project

---
+++ src/main/java/com/ta18/birdyard/BirdyardApplication.java
@@ -0,0 +1,13 @@
+package com.ta18.birdyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdyardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BirdyardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ta18/birdyard/BirdyardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ta18.birdyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdyardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
